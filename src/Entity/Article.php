42fa60c2e2b585eabc6e4e4995fc7091f7b393ca
sean <seanpeterson.997@gmail.com>
2024-04-18 14:09:35 -0600

    Article - Fill Entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,6 +2,9 @@
 
 namespace OHMedia\NewsBundle\Entity;
 
+use Doctrine\DBAL\Types\Types;
+use OHMedia\FileBundle\Entity\File;
+use OHMedia\MetaBundle\Entity\Meta;
 use OHMedia\NewsBundle\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
 use OHMedia\SecurityBundle\Entity\Traits\BlameableTrait;
@@ -16,6 +19,32 @@ class Article
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 100)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $author = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $snippet = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?File $image = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Meta $meta = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $publish_datetime = null;
+
     public function __toString(): string
     {
         return 'Article #'.$this->id;
@@ -24,5 +53,101 @@ class Article
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getSnippet(): ?string
+    {
+        return $this->snippet;
+    }
+
+    public function setSnippet(string $snippet): static
+    {
+        $this->snippet = $snippet;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage(): ?File
+    {
+        return $this->image;
+    }
+
+    public function setImage(File $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getMeta(): ?Meta
+    {
+        return $this->meta;
+    }
+
+    public function setMeta(Meta $meta): static
+    {
+        $this->meta = $meta;
+
+        return $this;
+    }
+
+    public function getPublishDatetime(): ?\DateTimeInterface
+    {
+        return $this->publish_datetime;
+    }
+
+    public function setPublishDatetime(\DateTimeInterface $publish_datetime): static
+    {
+        $this->publish_datetime = $publish_datetime;
+
+        return $this;
     }
 }
