42832795a43de7220e3d74ff0580ade120b4ab46
sean <seanpeterson.997@gmail.com>
2024-04-22 14:39:09 -0600

    Article Nav
    • Move the tags as a sub-nav item to articles

--- src/Service/ArticleNavItemProvider.php
+++ src/Service/ArticleNavItemProvider.php
@@ -7,14 +7,19 @@ use OHMedia\NewsBundle\Security\Voter\ArticleVoter;
 use OHMedia\BackendBundle\Service\AbstractNavItemProvider;
 use OHMedia\BootstrapBundle\Component\Nav\NavItemInterface;
 use OHMedia\BootstrapBundle\Component\Nav\NavLink;
+use OHMedia\BootstrapBundle\Component\Nav\NavDropdown;
 
 class ArticleNavItemProvider extends AbstractNavItemProvider
 {
     public function getNavItem(): ?NavItemInterface
     {
         if ($this->isGranted(ArticleVoter::INDEX, new Article())) {
-            return (new NavLink('Articles', 'article_index'))
-                ->setIcon('newspaper');
+            return (new NavDropdown('Articles', 'article_index'))
+                ->setIcon('newspaper')
+                ->addLink((new NavLink('Articles', 'article_index'))
+                    ->setIcon('newspaper'))
+                ->addLink((new NavLink('Tags', 'article_tag_index'))
+                    ->setIcon('tags'));
         }
 
         return null;
