0ee099f8c2c8ee365466afd8ca63fa21f36aec3e
sean <seanpeterson.997@gmail.com>
2024-04-22 14:51:12 -0600

    ArticleTag - fill out entity

--- src/Entity/ArticleTag.php
+++ src/Entity/ArticleTag.php
@@ -2,6 +2,8 @@
 
 namespace OHMedia\NewsBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use OHMedia\NewsBundle\Repository\ArticleTagRepository;
 use Doctrine\ORM\Mapping as ORM;
 use OHMedia\SecurityBundle\Entity\Traits\BlameableTrait;
@@ -16,6 +18,23 @@ class ArticleTag
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $slug = null;
+
+    /**
+     * @var Collection<int, Article>
+     */
+    #[ORM\ManyToMany(targetEntity: Article::class)]
+    private Collection $article;
+
+    public function __construct()
+    {
+        $this->article = new ArrayCollection();
+    }
+
     public function __toString(): string
     {
         return 'Article Tag #'.$this->id;
@@ -24,5 +43,53 @@ class ArticleTag
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Article>
+     */
+    public function getArticle(): Collection
+    {
+        return $this->article;
+    }
+
+    public function addArticle(Article $article): static
+    {
+        if (!$this->article->contains($article)) {
+            $this->article->add($article);
+        }
+
+        return $this;
+    }
+
+    public function removeArticle(Article $article): static
+    {
+        $this->article->removeElement($article);
+
+        return $this;
     }
 }
