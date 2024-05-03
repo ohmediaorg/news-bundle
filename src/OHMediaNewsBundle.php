4a1d44820d6edc3d83e004bf98c073b0b8793dd9
sean <seanpeterson.997@gmail.com>
2024-05-03 14:26:04 -0600

    Artile - New DIr Structure

--- src/OHMediaNewsBundle.php
+++ src/OHMediaNewsBundle.php
@@ -2,8 +2,17 @@
 
 namespace OHMedia\NewsBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class OHMediaNewsBundle extends Bundle
+class OHMediaNewsBundle extends AbstractBundle
 {
+    public function loadExtension(
+        array $config,
+        ContainerConfigurator $containerConfigurator,
+        ContainerBuilder $containerBuilder
+    ): void {
+        $containerConfigurator->import('../config/services.yaml');
+    }
 }
