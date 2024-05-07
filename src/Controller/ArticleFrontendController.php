2d91b73a1be27b6280c08937b194db6f99a136a0
sean <seanpeterson.997@gmail.com>
2024-05-07 15:46:16 -0600

    Article Item FE

--- src/Controller/ArticleFrontendController.php
+++ src/Controller/ArticleFrontendController.php
@@ -12,9 +12,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleFrontendController extends AbstractController
 {
     // TODO how do we handle the parent routes?
-    const PARENT_PATH = 'news';
+    public const PARENT_PATH = 'news';
 
-    #[Route('/' . self::PARENT_PATH, name: 'news')]
+    #[Route('/'.self::PARENT_PATH, name: 'news_listing')]
     public function listing(
         Request $request,
         Paginator $paginator,
@@ -29,6 +29,30 @@ class ArticleFrontendController extends AbstractController
         return $this->render('@OHMediaNews/article_listing.html.twig', [
             'pagination' => $paginator->paginate($qb, 8),
             'parent_path' => self::PARENT_PATH,
+        ]);
+    }
+
+    #[Route('/'.self::PARENT_PATH.'/{slug}', name: 'news_item')]
+    public function item(
+        Request $request,
+        ArticleRepository $articleRepository,
+        string $slug
+    ): Response {
+        $qb = $articleRepository->createQueryBuilder('a');
+        // TODO is there a filter equivalant for published?
+        $article = $qb->where('a.publish_datetime IS NOT NULL')
+            ->andWhere('a.slug = :slug')
+            ->setParameter('slug', $slug)
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        if(! $article) {
+            // TODO not found
+        }
+
+        return $this->render('@OHMediaNews/article_item.html.twig', [
+            'parent_path' => self::PARENT_PATH,
+            'article' => $article,
         ]);
     }
 }
