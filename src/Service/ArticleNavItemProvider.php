add1e04831308383c8f0cfedcbd5d5ce2e4c5793
sean <seanpeterson.997@gmail.com>
2024-05-29 15:03:11 -0600

    ArticleTag BE nav voter
    • Toggle article tags in the nav depending on if they are enabled

--- src/Service/ArticleNavItemProvider.php
+++ src/Service/ArticleNavItemProvider.php
@@ -2,26 +2,33 @@
 
 namespace OHMedia\NewsBundle\Service;
 
-use OHMedia\NewsBundle\Entity\Article;
-use OHMedia\NewsBundle\Security\Voter\ArticleVoter;
 use OHMedia\BackendBundle\Service\AbstractNavItemProvider;
+use OHMedia\BootstrapBundle\Component\Nav\NavDropdown;
 use OHMedia\BootstrapBundle\Component\Nav\NavItemInterface;
 use OHMedia\BootstrapBundle\Component\Nav\NavLink;
-use OHMedia\BootstrapBundle\Component\Nav\NavDropdown;
+use OHMedia\NewsBundle\Entity\Article;
+use OHMedia\NewsBundle\Entity\ArticleTag;
+use OHMedia\NewsBundle\Security\Voter\ArticleTagVoter;
+use OHMedia\NewsBundle\Security\Voter\ArticleVoter;
 
 class ArticleNavItemProvider extends AbstractNavItemProvider
 {
     public function getNavItem(): ?NavItemInterface
     {
+        $nav = null;
+
         if ($this->isGranted(ArticleVoter::INDEX, new Article())) {
-            return (new NavDropdown('Articles', 'article_index'))
+            $nav = (new NavDropdown('Articles', 'article_index'))
                 ->setIcon('newspaper')
                 ->addLink((new NavLink('Articles', 'article_index'))
-                    ->setIcon('newspaper'))
-                ->addLink((new NavLink('Tags', 'article_tag_index'))
+                    ->setIcon('newspaper'));
+
+            if ($this->isGranted(ArticleTagVoter::INDEX, new ArticleTag())) {
+                $nav->addLink((new NavLink('Tags', 'article_tag_index'))
                     ->setIcon('tags'));
+            }
         }
 
-        return null;
+        return $nav;
     }
 }
