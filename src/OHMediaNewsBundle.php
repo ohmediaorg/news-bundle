7087880818ff85973d2630aa30742b5780893f6d
sean <seanpeterson.997@gmail.com>
2024-10-08 16:25:51 -0600

    Restore article_tags container param

--- src/OHMediaNewsBundle.php
+++ src/OHMediaNewsBundle.php
@@ -2,17 +2,32 @@
 
 namespace OHMedia\NewsBundle;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class OHMediaNewsBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+                ->booleanNode('article_tags')
+                    ->defaultTrue()
+                ->end()
+            ->end();
+    }
+
     public function loadExtension(
         array $config,
         ContainerConfigurator $containerConfigurator,
         ContainerBuilder $containerBuilder
     ): void {
         $containerConfigurator->import('../config/services.yaml');
+
+        $containerConfigurator->parameters()
+            ->set('oh_media_news.article_tags', $config['article_tags'])
+        ;
     }
 }
