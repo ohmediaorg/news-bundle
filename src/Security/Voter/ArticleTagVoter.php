2fdb42812ca2087081a5699eff24e81c7f7294d7
sean <seanpeterson.997@gmail.com>
2024-10-10 11:38:06 -0600

    ArticleTag - Delete protection
    Can only delete IF no article relations exist.

--- src/Security/Voter/ArticleTagVoter.php
+++ src/Security/Voter/ArticleTagVoter.php
@@ -57,7 +57,7 @@ class ArticleTagVoter extends AbstractEntityVoter
 
     protected function canDelete(ArticleTag $articleTag, User $loggedIn): bool
     {
-        // TODO check if relations exist first?
-        return $this->enabledArticleTags;
+        return $this->enabledArticleTags
+            && 0 === sizeof($articleTag->getArticles());
     }
 }
