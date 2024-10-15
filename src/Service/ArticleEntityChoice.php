cf70eb2ab5fe7623e37aac43056355d4b231322c
sean <seanpeterson.997@gmail.com>
2024-10-15 12:47:39 -0600

    Revert "Seperate ArticleTagEntityChoice From Article"
    This reverts commit 6a3299c44c14faaf5c214a882b3a6e863155c72a.

--- src/Service/ArticleEntityChoice.php
+++ src/Service/ArticleEntityChoice.php
@@ -3,6 +3,7 @@
 namespace OHMedia\NewsBundle\Service;
 
 use OHMedia\NewsBundle\Entity\Article;
+use OHMedia\NewsBundle\Entity\ArticleTag;
 use OHMedia\SecurityBundle\Service\EntityChoiceInterface;
 
 class ArticleEntityChoice implements EntityChoiceInterface
@@ -12,10 +13,12 @@ class ArticleEntityChoice implements EntityChoiceInterface
         return 'Articles';
     }
 
+    // TODO - might want seperate permissions
     public function getEntities(): array
     {
         return [
             Article::class,
+            ArticleTag::class,
         ];
     }
 }
