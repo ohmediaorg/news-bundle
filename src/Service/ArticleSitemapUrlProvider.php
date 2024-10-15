3dbfe300f4e5dcd04619f16c540b70611b40fab6
sean <seanpeterson.997@gmail.com>
2024-10-15 12:24:27 -0600

    Sitemap Service

---
+++ src/Service/ArticleSitemapUrlProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OHMedia\NewsBundle\Service;
+
+use OHMedia\NewsBundle\Repository\ArticleRepository;
+use OHMedia\PageBundle\Service\PageRawQuery;
+use OHMedia\PageBundle\Sitemap\AbstractSitemapUrlProvider;
+
+class ArticleSitemapUrlProvider extends AbstractSitemapUrlProvider
+{
+    public function __construct(
+        private ArticleRepository $articleRepository,
+        private PageRawQuery $pageRawQuery,
+    ) {
+    }
+
+    protected function buildSitemapUrls(): void
+    {
+        $pagePath = $this->pageRawQuery->getPathWithShortcode('news()');
+
+        if (!$pagePath) {
+            return;
+        }
+
+        $articles = $this->articleRepository->getPublishedArticles()
+            ->getQuery()
+            ->getResult();
+
+        foreach ($articles as $article) {
+            $this->addSitemapUrl(
+                $pagePath.'/'.$article->getSlug(),
+                $article->getUpdatedAt()
+            );
+        }
+    }
+}
