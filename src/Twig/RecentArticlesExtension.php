2464d748c5ea9b483678022abd0f379514171d03
sean <seanpeterson.997@gmail.com>
2024-10-15 13:00:52 -0600

    Twig - Recent Articles
    Recent Articles Twig function
     - List out the 3 most recent articles

---
+++ src/Twig/RecentArticlesExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace OHMedia\NewsBundle\Twig;
+
+use OHMedia\NewsBundle\Repository\ArticleRepository;
+use OHMedia\PageBundle\Service\PageRawQuery;
+use OHMedia\WysiwygBundle\Twig\AbstractWysiwygExtension;
+use Twig\Environment;
+use Twig\TwigFunction;
+
+class RecentArticlesExtension extends AbstractWysiwygExtension
+{
+    public function __construct(
+        private ArticleRepository $articleRepository,
+        private PageRawQuery $pageRawQuery,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('recent_articles', [$this, 'recentArticles'], [
+                'needs_environment' => true,
+                'is_safe' => ['html'],
+            ]),
+        ];
+    }
+
+    public function recentArticles(Environment $twig, int $limit = 3): string
+    {
+        if ($limit <= 0) {
+            $limit = 3;
+        }
+
+        $qb = $this->articleRepository->getPublishedArticles();
+        $qb->setMaxResults($limit);
+
+        $articles = $qb->getQuery()->getResult();
+
+        $pagePath = $this->pageRawQuery->getPathWithShortcode('news()');
+
+        return $twig->render('@OHMediaNews/recent_news.html.twig', [
+            'articles' => $articles,
+            'news_page_path' => $pagePath,
+        ]);
+    }
+}
