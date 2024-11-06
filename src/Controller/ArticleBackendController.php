348f5093e92493b916b32754cafb9256bd08cab4
sean <seanpeterson.997@gmail.com>
2024-11-06 11:16:22 -0600

    ArticleRssBackendController - Missing redirect

--- src/Controller/ArticleBackendController.php
+++ src/Controller/ArticleBackendController.php
@@ -14,7 +14,6 @@ use OHMedia\UtilityBundle\Form\DeleteType;
 use OHMedia\UtilityBundle\Service\EntitySlugger;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
