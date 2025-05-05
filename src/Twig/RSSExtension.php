6965c0b2fc100897b23059c41562d2853bb4865f
Justin Hoffman <justin@ohmedia.ca>
2025-05-05 16:43:31 -0600

    add extension for twig function to render the RSS link tag

---
+++ src/Twig/RSSExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace OHMedia\NewsBundle\Twig;
+
+use OHMedia\NewsBundle\Entity\Article;
+use OHMedia\SettingsBundle\Service\Settings;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class RSSExtension extends AbstractExtension
+{
+    public function __construct(
+        private Settings $settings,
+        private UrlGeneratorInterface $urlGenerator,
+    ) {
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('news_rss_link_tag', [$this, 'rssLinkTag'], [
+                'is_safe' => ['html'],
+            ]),
+        ];
+    }
+
+    public function rssLinkTag(): string
+    {
+        $title = $this->settings->get(Article::SETTING_RSS_TITLE);
+        $href = $this->urlGenerator->generate('news_rss');
+
+        return sprintf(
+            '<link rel="alternate" type="application/rss+xml" title="%s" href="%s">',
+            htmlspecialchars($title),
+            $href,
+        );
+    }
+}
